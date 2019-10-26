893163e74be36e62fb303b9d809b6c72dce9ff8a
Kurbonov, Beck <otabekkurbonov1995@gmail.com>
2019-10-25 20:16:24 -0400

    2

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
