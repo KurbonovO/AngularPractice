d5384bc5e19ca81544064d8134b746ce6b409c7c
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2019-11-30 20:37:28 -0500

    22 assigemnt 1

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
